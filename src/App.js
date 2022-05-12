346454373d9c3c24fe806c1b4876e1204877301a
nguyenminhsang2004 <67231653+nguyenminhsang2004@users.noreply.github.com>
2022-05-12 21:09:40 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,28 +3,6 @@ import RandomImage from "./components/RandomImage";
 import { useState } from "react";
 
 function App() {
-  const [todoList, setTodoList] = useState([
-    { id: 1, title: "ReactJS" },
-    { id: 2, title: "ReactHooks" },
-    { id: 3, title: "NextJS" },
-  ]);
-  const handleTodoClick = (todo) => {
-    const index = todoList.findIndex((x) => x.id === todo.id);
-    if (index < 0) return;
-    const newTodoList = [...todoList];
-    newTodoList.splice(index, 1);
-    setTodoList(newTodoList);
-  };
-
-  const handleTodoFormSubmit = (formValues) => {
-    const newTodo = {
-      id: todoList.length + 1,
-      ...formValues,
-    };
-    const newTodoList = [...todoList];
-    newTodoList.push(newTodo);
-    setTodoList(newTodoList);
-  };
   return (
     <div className="App">
       <RandomImage />
